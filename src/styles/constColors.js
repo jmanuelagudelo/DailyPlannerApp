c05d259194ca2b4115e92efc8acf191223f9ac00
Manuel Agudelo <121588920+jmanuelagudelo@users.noreply.github.com>
2023-07-05 20:45:32 -0500

    Constantes Colores1

---
+++ src/styles/constColors.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text} from "react-native";
+
+const Colors1 = {
+    fondoColor: "#ffe7cb",
+    primaryColor: "#dd704f",
+    secondaryColor: "#5b8190",
+    iconsColor: "#588c7e",
+};
+export default Colors1;
